a74b47db5cd08d7a0fd7a7b0c5145f0257ae26b6
Luis <Luis@10.1.4.117>
2015-03-03 11:57:49 +0100

    Versión provisional lab0

--- src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
+++ src/main/java/es/unizar/tmdad/lab0/controller/SearchController.java
@@ -1,10 +1,12 @@
 package es.unizar.tmdad.lab0.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.unizar.tmdad.lab0.service.TwitterLookupService;
 
@@ -21,8 +23,8 @@ public class SearchController {
     }
 
     @RequestMapping("/search")
-    public String search(@RequestParam("q") String q, Model m) {
-    	m.addAttribute("res", twitter.search(q));
-        return "search :: content";
+    @ResponseBody
+    public SearchResults search(@RequestParam("q") String q, Model m) {
+        return twitter.search(q);
     }
 }
